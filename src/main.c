493e92c2e725a0ee45a294a92bbaee4f33b349a5
Serosh <janmejayapanda400@gmail.com>
2026-02-05 20:52:16 +0530

    Update main entry point to use application startup signal

--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@ int main(int argc, char *argv[]) {
     int status;
 
     app = gtk_application_new("org.aetherfm.app", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "startup", G_CALLBACK(aetherfm_app_startup), NULL);
     g_signal_connect(app, "activate", G_CALLBACK(aetherfm_window_activate), NULL);
+    
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
 
